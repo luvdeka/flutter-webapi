0be7a1126c06918555a54f3d7f8ec2816985766f
DAVI GOMES KORC <davi.0931@aluno.pr.senac.br>
2024-03-04 16:26:02 -0300

    alteração do tema e fonte

---
+++ flutter-webapi-main/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const appBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          tittletextStyle: TextStyle(
+            color:Colors.white,
+          ),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme()
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
